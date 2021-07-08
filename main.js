5f2ad063837805f8e944501baf27c52cb1296708
jeneljenel <jwwlee@gmail.com>
2021-07-08 14:59:04 +0200

    Adding QR scan functionality

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 function state() {
     return {
-        hello: "hello"
+        user: {
+            showQRScanner: false,
+            stationsVisited: [],
+        },
+        showQRScanner: function() {
+            this.user.showQRScanner = true;
+            scanQRCode(audio_id => {
+                this.tryStory(audio_id);
+            });
+        },
+        tryStory: function(audio_id) {
+            let user = this.user;
+            let state = this;
+            let visited = [];
+
+            // Will add the story to user data
+            for (var i = 0; i < user.stationsVisited.length; i++) {
+                visited.push(user.stationsVisited[i].id)
+            }
+            
+        }
     }
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.initQR();
+});
 
 window.state = state;
---
+++ qrscanner.js
@@ -0,0 +1,40 @@
+window.initQR = function (){
+    let canvasElement = document.getElementById("qr-canvas");
+    window.qr = {
+      video: document.createElement("video"),
+      canvasElement: canvasElement,
+      canvas: canvasElement.getContext("2d"),
+      scanning: false
+    };
+  };
+  
+window.scanQRCode = function(callback) {
+  let qr = window.qr;
+  window.qrcode.callback = callback;
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      qr.scanning = true;
+      qr.canvasElement.hidden = false;
+      qr.video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      qr.video.srcObject = stream;
+      qr.video.play();
+      tick();
+      scan();
+    });
+};
+
+function tick() {
+  qr.canvasElement.height = qr.video.videoHeight;
+  qr.canvasElement.width = qr.video.videoWidth;
+  qr.canvas.drawImage(qr.video, 0, 0, qr.canvasElement.width, qr.canvasElement.height);
+  qr.scanning && requestAnimationFrame(tick);
+};
+
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+};  
