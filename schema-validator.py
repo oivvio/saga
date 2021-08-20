96c9489aad8f7b4fe43197d4a54146c2c9c71626
jeneljenel <jwwlee@gmail.com>
2021-08-20 15:03:36 +0200

    Validation schema almost done - Added conditionally validation for triggers
    ToDo:
    - Maybe use recursion to be applied on next level reusing triggers

--- schema-validator.py
+++ schema-validator.py
@@ -1,16 +1,32 @@
+from os import DirEntry
 from jsonschema import validate
 from jsonschema import  Draft7Validator
 from json import load
 
+import os
+
+directory = "./data/stations"
+
 with open('schema.json') as file:
     schema = load(file)
 
-with open('./data/stations/schema-test.json') as file:
-    code_to_check = load(file)
+with open(directory + "/schema-test.json") as f:
+    code_to_check = load(f)    
+
+# # Run all files in directory
+# for file in os.scandir(directory):
+#     if file.path.endswith(".json"):
+#         with open(file.path) as f:
+#             code_to_check = load(f)    
+
+#         validator = Draft7Validator(schema)
+#         print(list(validator.iter_errors(code_to_check)), "\n")     
+
 
 # Only show errors
 validator = Draft7Validator(schema)
-print(list(validator.iter_errors(code_to_check)))
+print(list(validator.iter_errors(code_to_check)), "\n")
+
 
 # Run all
 # validate(code_to_check, schema=schema)
