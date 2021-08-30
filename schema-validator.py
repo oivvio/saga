a647485399cbe04182ec6e4152378b19beccebaa
Oivvio Polite <oivvio@liberationtech.net>
2021-08-30 11:07:43 +0200

    Minor changes to validation

--- schema-validator.py
+++ schema-validator.py
@@ -1,26 +1,26 @@
 from os import DirEntry
 from jsonschema import validate
-from jsonschema import  Draft7Validator
+from jsonschema import Draft7Validator
 from json import load
 
 import os
 
-directory = "./data/stations"
+directory = "./src/data/stations"
 
-with open('./schemas/schema.json') as file:
+with open("./schemas/schema.json") as file:
     schema = load(file)
 
 with open(directory + "/schema-test.json") as f:
-    code_to_check = load(f)    
+    code_to_check = load(f)
 
 # # Run all files in directory
 # for file in os.scandir(directory):
 #     if file.path.endswith(".json"):
 #         with open(file.path) as f:
-#             code_to_check = load(f)    
+#             code_to_check = load(f)
 
 #         validator = Draft7Validator(schema)
-#         print(list(validator.iter_errors(code_to_check)), "\n")     
+#         print(list(validator.iter_errors(code_to_check)), "\n")
 
 
 # Only show errors
