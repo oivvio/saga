7ab7497fd6ec374766e34f633840837734a6ee92
Oivvio Polite <oivvio@liberationtech.net>
2021-08-30 18:28:11 +0200

    added a multi file validator task

--- tasks.py
+++ tasks.py
@@ -1,16 +1,14 @@
 import os
+import sys
 from invoke import task
+from pathlib import Path
 
-from os import DirEntry
-from jsonschema import validate
 from jsonschema import Draft7Validator
 from json import load
 
-import sys
-
 
 def preflight_checklist():
-    """Stuff we want to check before running our tasks """
+    """ Stuff we want to check before running our tasks """
 
     # Check that we are in the right folder
     if not os.path.exists("tasks.py"):
@@ -19,17 +17,52 @@ def preflight_checklist():
 
 
 def validate_station_file(filename):
-    with open("./schemas/schema.json") as file:
-        schema = load(file)
+    """Validate a given file against our schema a return a list of errors"""
+    with open("./schemas/schema.json") as handle:
+        schema = load(handle)
 
-    print(schema)
-    print(filename)
+    with open(filename) as handle:
+        json_to_check = load(handle)
+
+    validator = Draft7Validator(schema)
+
+    errors = [e for e in validator.iter_errors(json_to_check)]
+    return errors
+
+
+def output_validation_errors(errors, filename):
+    """ Output validation errors and filename for human consumption """
+    if errors:
+        print("=" * 100)
+        print(filename)
+    for error in errors:
+        print("-" * 100)
+        print(f"{error.validator=}")
+        print(f"{error.message=}")
 
 
 @task
 def validate_test_file(ctx):
-    """ Test the schema validation """
+    """ Test the schema validation against a test file"""
     preflight_checklist()
     jsonfile = "./src/data/stations/schema-test.json"
 
     validate_station_file(jsonfile)
+
+
+@task
+def validate_stations_in_folder(ctx, folder, exit_on_errors=False):
+    """
+    Validate all json files in given folder
+
+    If exit_on_error only print errors of first file that does not validate, then exit
+    """
+    preflight_checklist()
+    filenames = [fn for fn in Path(folder).iterdir() if fn.as_posix().endswith(".json")]
+    for filename in filenames:
+        errors = validate_station_file(filename)
+        if errors:
+            output_validation_errors(errors, filename)
+            if exit_on_errors:
+                exit()
+            errors = None
